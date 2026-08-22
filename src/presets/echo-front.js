435c9b927a182d98c8e9d487cabd812de8965fd3
egor125552 <sitkosergej55@gmail.com>
2026-08-22 13:30:53 +0300

    Wire first-round training plugin

--- src/presets/echo-front.js
+++ src/presets/echo-front.js
@@ -6,12 +6,13 @@ import * as teams from "../plugins/teams/server.js";
 import * as health from "../plugins/health/server.js";
 import * as armor from "../plugins/armor/server.js";
 import * as spawnProtection from "../plugins/spawn-protection/server.js";
+import * as tdm from "../plugins/tdm/server.js";
+import * as trainingRound from "../plugins/training-round/server.js";
 import * as aimAssist from "../plugins/aim-assist/server.js";
 import * as combat from "../plugins/combat/server.js";
 import * as weapons from "../plugins/weapons/server.js";
 import * as weaponProgression from "../plugins/weapon-progression/server.js";
 import * as respawn from "../plugins/respawn/server.js";
-import * as tdm from "../plugins/tdm/server.js";
 import * as botController from "../plugins/bot-controller/server.js";
 import * as botLoadouts from "../plugins/bot-loadouts/server.js";
 import * as botPerception from "../plugins/bot-perception/server.js";
@@ -28,12 +29,13 @@ export const echoFrontPreset = [
   health,
   armor,
   spawnProtection,
+  tdm,
+  trainingRound,
   aimAssist,
   combat,
   weapons,
   weaponProgression,
   respawn,
-  tdm,
   botController,
   botLoadouts,
   botPerception,
