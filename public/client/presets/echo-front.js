6cf8e3c0bd6bf3f9eda6651ebf550b75aceec3e5
egor125552 <sitkosergej55@gmail.com>
2026-08-22 14:12:14 +0300

    Start public play journal before snapshot smoothing

--- public/client/presets/echo-front.js
+++ public/client/presets/echo-front.js
@@ -1,7 +1,7 @@
 import * as input from "../plugins/input.js";
 import * as network from "../plugins/network.js";
-import * as smoothing from "../plugins/snapshot-smoothing.js";
 import * as journal from "../plugins/play-journal.js";
+import * as smoothing from "../plugins/snapshot-smoothing.js";
 import * as speechSettings from "../plugins/speech-settings.js";
 import * as spatialAudio from "../plugins/spatial-audio.js";
 import * as soundPack from "../plugins/core-sound-pack.js";
@@ -11,8 +11,8 @@ import * as announcer from "../plugins/announcer.js";
 export const echoFrontClientPreset = [
   input,
   network,
-  smoothing,
   journal,
+  smoothing,
   speechSettings,
   spatialAudio,
   soundPack,
