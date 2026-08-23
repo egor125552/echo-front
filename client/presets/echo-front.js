cd72ecb1820f3397ff81dca94fad6cb2e509e8ad
egor125552 <sitkosergej55@gmail.com>
2026-08-24 01:03:18 +0300

    Enable low health audio effects

--- client/presets/echo-front.js
+++ client/presets/echo-front.js
@@ -6,6 +6,7 @@ import * as speechSettings from "../plugins/speech-settings.js";
 import * as spatialAudio from "../plugins/spatial-audio.js";
 import * as soundPack from "../plugins/core-sound-pack.js";
 import * as environmentAudio from "../plugins/environment-audio.js";
+import * as lowHealthAudio from "../plugins/low-health-audio.js";
 import * as hud from "../plugins/game-hud.js";
 import * as announcer from "../plugins/announcer.js";
 
@@ -18,6 +19,7 @@ export const echoFrontClientPreset = [
   spatialAudio,
   soundPack,
   environmentAudio,
+  lowHealthAudio,
   hud,
   announcer,
 ];
