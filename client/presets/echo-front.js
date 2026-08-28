29a9c8f1b79164b5bf722cea0538fefdf4751c75
egor125552 <sitkosergej55@gmail.com>
2026-08-28 14:39:42 +0300

    Enable navigation announcer

--- client/presets/echo-front.js
+++ client/presets/echo-front.js
@@ -22,6 +22,7 @@ import * as parachuteDynamicsAudio from "../plugins/parachute-dynamics-audio.js"
 import * as lowHealthAudio from "../plugins/low-health-audio.js";
 import * as hud from "../plugins/game-hud.js";
 import * as announcer from "../plugins/announcer.js";
+import * as navigationAnnouncer from "../plugins/battle-royale-navigation-announcer.js";
 import * as parachuteAnnouncer from "../plugins/parachute-announcer.js";
 import * as vehicleAnnouncer from "../plugins/battle-royale-vehicle-announcer.js";
 
@@ -50,6 +51,7 @@ export const echoFrontClientPreset = [
   lowHealthAudio,
   hud,
   announcer,
+  navigationAnnouncer,
   parachuteAnnouncer,
   vehicleAnnouncer,
 ];
