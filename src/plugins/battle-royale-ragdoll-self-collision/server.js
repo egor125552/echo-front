c4a1d53c23fe6f6acce360deea4e422a406a27d7
egor125552 <sitkosergej55@gmail.com>
2026-08-28 17:22:34 +0300

    Add ragdoll self collision realism plugin

---
+++ src/plugins/battle-royale-ragdoll-self-collision/server.js
@@ -0,0 +1,12 @@
+export const manifest = {
+  id: "battle-royale-ragdoll-self-collision",
+  version: "1.0.0",
+  requires: ["battle-royale-ragdoll-stability"],
+  capabilities: ["services.consume"],
+};
+
+export async function setup(ctx) {
+  const stability = ctx.services.get("ragdoll-stability");
+  stability.setSelfCollisionEnabled(true);
+  ctx.events.emit("ragdoll:self-collision-enabled", { enabled: true });
+}
