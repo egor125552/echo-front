0f015ca5686dae43cc290dd45305950f1b0ffd7f
egor125552 <sitkosergej55@gmail.com>
2026-08-29 13:42:48 +0300

    Enable one-tap navigation facing

--- src/presets/battle-royale.js
+++ src/presets/battle-royale.js
@@ -52,6 +52,7 @@ import * as botParachute from "../plugins/battle-royale-bot-parachute/server.js"
 import * as ragdollIntegration from "../plugins/battle-royale-ragdoll-integration/server.js";
 import * as parkourRagdoll from "../plugins/battle-royale-parkour-ragdoll/server.js";
 import * as navigation from "../plugins/battle-royale-navigation/index.js";
+import * as navigationFace from "../plugins/battle-royale-navigation-face/server.js";
 import * as dropzoneVehicle from "../plugins/battle-royale-dropzone-vehicle/server.js";
 
 export const battleRoyalePreset = [
@@ -109,5 +110,6 @@ export const battleRoyalePreset = [
   ragdollIntegration,
   parkourRagdoll,
   navigation,
+  navigationFace,
   dropzoneVehicle,
 ];
