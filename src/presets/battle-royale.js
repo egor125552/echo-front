ccf33327940a04f08c75e8b9db67ea60cf12ea1f
egor125552 <sitkosergej55@gmail.com>
2026-08-29 21:13:27 +0300

    Enable cardinal staircase physics in Battle Royale

--- src/presets/battle-royale.js
+++ src/presets/battle-royale.js
@@ -1,5 +1,6 @@
 import * as entities from "../plugins/entities/server.js";
 import * as physics from "../plugins/rapier-physics/server.js";
+import * as cardinalRamps from "../plugins/rapier-cardinal-ramps/server.js";
 import * as ragdollStability from "../plugins/battle-royale-ragdoll-stability/server.js";
 import * as ragdollSelfCollision from "../plugins/battle-royale-ragdoll-self-collision/server.js";
 import * as liveCharacterQueries from "../plugins/rapier-live-character-queries/server.js";
@@ -64,6 +65,7 @@ import * as dropzoneVehicle from "../plugins/battle-royale-dropzone-vehicle/serv
 export const battleRoyalePreset = [
   entities,
   physics,
+  cardinalRamps,
   ragdollStability,
   ragdollSelfCollision,
   liveCharacterQueries,
