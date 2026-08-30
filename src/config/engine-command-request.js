00232288e920769855c606f71289b26f5605505e
egor125552 <sitkosergej55@gmail.com>
2026-08-30 22:24:01 +0300

    Probe speed-scaled vehicle ejection ragdoll

--- src/config/engine-command-request.js
+++ src/config/engine-command-request.js
@@ -1,28 +1,33 @@
-const START = 2000021600000;
-const PLAYER = "engine-nav-sanity-216";
+const START = 2000021700000;
+const CASES = Object.freeze([
+  { player: "ragdoll-speed-15", speedKph: 15, x: -24 },
+  { player: "ragdoll-speed-50", speedKph: 50, x: -8 },
+  { player: "ragdoll-speed-100", speedKph: 100, x: 8 },
+  { player: "ragdoll-speed-150", speedKph: 150, x: 24 },
+]);
+
+const commands = [];
+for (const [index, test] of CASES.entries()) {
+  const speed = test.speedKph / 3.6;
+  commands.push(
+    { command: "service.call", args: { service: "match-api", method: "connectHuman", arguments: [test.player] } },
+    { command: "service.call", args: { service: "movement", method: "teleport", arguments: [test.player, { x: test.x, y: 30, z: -60, angle: 0 }] } },
+    { command: "service.call", args: { service: "ragdoll", method: "activate", arguments: [test.player, { reason: "vehicle-eject", velocity: { x: speed, y: 5, z: 0 } }, START + index] } },
+    { command: "service.call", args: { service: "ragdoll-tuning", method: "stateFor", arguments: [test.player] } },
+  );
+}
+commands.push(
+  { command: "service.call", args: { service: "ragdoll-tuning", method: "currentReason", arguments: ["vehicle-eject"] } },
+  { command: "service.call", args: { service: "ragdoll-stability", method: "summary", arguments: [] } },
+);
 
 export const ENGINE_COMMAND_REQUEST = Object.freeze({
-  id: 216,
+  id: 217,
   mode: "battle-royale",
-  room: "engine-nav-sanity-216",
+  room: "ragdoll-speed-scaling-217",
   command: "engine.batch",
   repeat: 1,
   frameEvery: 1,
-  args: { commands: [
-    { command: "service.call", args: { service: "match-api", method: "connectHuman", arguments: [PLAYER] } },
-    { command: "service.call", args: { service: "movement", method: "teleport", arguments: [PLAYER, { x: 151.5, y: 0, z: 114.8, angle: 0 }] } },
-    { command: "service.call", args: { service: "parachute", method: "finishMovement", arguments: [0.016, START] } },
-    { command: "service.call", args: { service: "navigation", method: "selectTarget", arguments: [PLAYER, "two-storey-house", START + 1, { announce: false }] } },
-    { command: "service.call", args: { service: "navigation", method: "toggle", arguments: [PLAYER, START + 2] } },
-    { command: "service.call", args: { service: "navigation", method: "stateFor", arguments: [PLAYER, START + 3] } },
-    { command: "service.call", args: { service: "navigation", method: "stop", arguments: [PLAYER, START + 4, "probe-one", { announce: false }] } },
-
-    { command: "service.call", args: { service: "movement", method: "teleport", arguments: [PLAYER, { x: 133.5, y: 0, z: 115.36, angle: 0 }] } },
-    { command: "service.call", args: { service: "navigation", method: "selectTarget", arguments: [PLAYER, "two-storey-house", START + 5, { announce: false }] } },
-    { command: "service.call", args: { service: "navigation", method: "toggle", arguments: [PLAYER, START + 6] } },
-    { command: "service.call", args: { service: "navigation", method: "stateFor", arguments: [PLAYER, START + 7] } },
-    { command: "service.call", args: { service: "navigation", method: "stop", arguments: [PLAYER, START + 8, "probe-two", { announce: false }] } },
-    { command: "service.call", args: { service: "match-api", method: "disconnectHuman", arguments: [PLAYER] } }
-  ] },
-  requestedAt: "2026-08-30T21:51:00+03:00"
+  args: { commands },
+  requestedAt: "2026-08-30T22:21:00+03:00"
 });
