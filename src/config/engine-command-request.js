422f81fe9c95d88c767e3183318b77dffb87c62f
egor125552 <sitkosergej55@gmail.com>
2026-08-30 21:50:40 +0300

    Reproduce pathological short navigation routes

--- src/config/engine-command-request.js
+++ src/config/engine-command-request.js
@@ -1,27 +1,28 @@
-const START = 2000021500000;
-const PLAYER = "engine-vehicle-door-215";
-const LANDING = { x: 150, y: 0, z: 120 };
+const START = 2000021600000;
+const PLAYER = "engine-nav-sanity-216";
 
 export const ENGINE_COMMAND_REQUEST = Object.freeze({
-  id: 215,
+  id: 216,
   mode: "battle-royale",
-  room: "engine-vehicle-door-215",
+  room: "engine-nav-sanity-216",
   command: "engine.batch",
   repeat: 1,
   frameEvery: 1,
   args: { commands: [
     { command: "service.call", args: { service: "match-api", method: "connectHuman", arguments: [PLAYER] } },
-    { command: "service.call", args: { service: "movement", method: "teleport", arguments: [PLAYER, { ...LANDING, angle: 0 }] } },
+    { command: "service.call", args: { service: "movement", method: "teleport", arguments: [PLAYER, { x: 151.5, y: 0, z: 114.8, angle: 0 }] } },
     { command: "service.call", args: { service: "parachute", method: "finishMovement", arguments: [0.016, START] } },
-    { command: "service.call", args: { service: "dropzone-vehicle", method: "placeNear", arguments: [PLAYER, LANDING, START + 1] } },
-    { command: "service.call", args: { service: "dropzone-vehicle", method: "assignedFor", arguments: [PLAYER] } },
-    { command: "service.call", args: { service: "movement", method: "teleport", arguments: [PLAYER, { x: 160.5, y: 0, z: 120, angle: 0 }] } },
-    { command: "service.call", args: { service: "vehicles", method: "enter", arguments: [PLAYER, START + 2, "br-supercar-1"] } },
-    { command: "service.call", args: { service: "navigation", method: "selectTarget", arguments: [PLAYER, "two-storey-house", START + 3, { announce: false }] } },
-    { command: "service.call", args: { service: "navigation", method: "toggle", arguments: [PLAYER, START + 4] } },
-    { command: "service.call", args: { service: "navigation", method: "stateFor", arguments: [PLAYER, START + 5] } },
-    { command: "service.call", args: { service: "vehicles", method: "exit", arguments: [PLAYER, START + 6, "engine-control"] } },
+    { command: "service.call", args: { service: "navigation", method: "selectTarget", arguments: [PLAYER, "two-storey-house", START + 1, { announce: false }] } },
+    { command: "service.call", args: { service: "navigation", method: "toggle", arguments: [PLAYER, START + 2] } },
+    { command: "service.call", args: { service: "navigation", method: "stateFor", arguments: [PLAYER, START + 3] } },
+    { command: "service.call", args: { service: "navigation", method: "stop", arguments: [PLAYER, START + 4, "probe-one", { announce: false }] } },
+
+    { command: "service.call", args: { service: "movement", method: "teleport", arguments: [PLAYER, { x: 133.5, y: 0, z: 115.36, angle: 0 }] } },
+    { command: "service.call", args: { service: "navigation", method: "selectTarget", arguments: [PLAYER, "two-storey-house", START + 5, { announce: false }] } },
+    { command: "service.call", args: { service: "navigation", method: "toggle", arguments: [PLAYER, START + 6] } },
+    { command: "service.call", args: { service: "navigation", method: "stateFor", arguments: [PLAYER, START + 7] } },
+    { command: "service.call", args: { service: "navigation", method: "stop", arguments: [PLAYER, START + 8, "probe-two", { announce: false }] } },
     { command: "service.call", args: { service: "match-api", method: "disconnectHuman", arguments: [PLAYER] } }
   ] },
-  requestedAt: "2026-08-30T21:18:00+03:00"
+  requestedAt: "2026-08-30T21:51:00+03:00"
 });
