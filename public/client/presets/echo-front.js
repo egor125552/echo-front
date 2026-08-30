e3f2c26b1fdc1ff47e402276d34207cb82e8b53f
egor125552 <sitkosergej55@gmail.com>
2026-08-30 16:16:53 +0300

    Enable map menu activation bridge

--- public/client/presets/echo-front.js
+++ public/client/presets/echo-front.js
@@ -14,6 +14,7 @@ import * as speechDiagnostics from "../plugins/speech-diagnostics.js";
 import * as spatialAudio from "../plugins/spatial-audio.js";
 import * as audioResilience from "../plugins/audio-resilience.js";
 import * as navigation from "../plugins/battle-royale-navigation.js";
+import * as mapMenuActivation from "../plugins/map-menu-activation.js";
 import * as navigationStatus from "../plugins/battle-royale-navigation-status.js";
 import * as navigationFace from "../plugins/battle-royale-navigation-face.js";
 import * as parachuteAudioPreload from "../plugins/parachute-audio-preload.js";
@@ -52,6 +53,7 @@ export const echoFrontClientPreset = [
   spatialAudio,
   audioResilience,
   navigation,
+  mapMenuActivation,
   navigationStatus,
   navigationFace,
   parachuteAudioPreload,
